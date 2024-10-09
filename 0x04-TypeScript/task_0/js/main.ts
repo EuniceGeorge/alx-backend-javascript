ec508fd473de28e6c2ad8a837cb76a05b22ac166
EuniceGeorge <georgeunice408.com>
2024-10-09 18:16:23 +0100

    uapdate

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+       	age: number;
+	location: string;
+}
+
+const student1: Student = {
+	firstName: "Ruth";
+        lastName: "Kadiri";
+        age: 30;
+        location: "Lagos";
+};
+
+const student2: Student = {
+        firstName: "Bukky";
+        lastName: "Wright";
+        age: 40;
+        location: "Abuja";
+};
+
+let studentsList[] = [student1, student2]
