f6347a9408b6122c934b150540adc3d6733aad6c
EuniceGeorge <georgeunice408.com>
2024-11-27 05:10:09 +0100

    task

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+// method that accepts a path in argument and returns the number of students in the file given
+
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const nstudent = fs.readFileSync(path, 'utf8').split('\n');
+    const nstudents = nstudent.slice(1);
+    const filteredStudents = nstudents.filter((item) => item);
+    console.log(`Number of students: ${filteredStudents.length}`);
+    const fields = {};
+    for (const i of filteredStudents) {
+      const student = i.split(',');
+      if (!fields[student[3]]) {
+        fields[student[3]] = [];
+      }
+      fields[student[3]].push(student[0]);
+    }
+    for (const key of Object.keys(fields)) {
+      if (key) {
+        const list = fields[key];
+        console.log(`Number of students in ${key}: ${list.length}. List: ${list.join(', ')}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+// function that accepts a file path in argument and attempts to read the file asynchronously
+
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database asynchronously'));
+        return;
+      }
+
+      const nstudents = data.split('\n').slice(1).filter((item) => item);
+      console.log(`Number of students: ${nstudents.length}`);
+
+      const fields = {};
+      for (const i of nstudents) {
+        const student = i.split(',');
+        if (!fields[student[3]]) {
+          fields[student[3]] = [];
+        }
+        fields[student[3]].push(student[0]);
+      }
+
+      for (const key of Object.keys(fields)) {
+        if (key) {
+          const list = fields[key];
+          console.log(`Number of students in ${key}: ${list.length}. List: ${list.join(', ')}`);
+        }
+      }
+
+      resolve(); // Resolve the Promise when the operation is complete
+    });
+  });
+}
+
+module.exports = countStudents;
